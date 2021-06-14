48e70e872d7e4264f3c502ecf7e619a0ca423027
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-06-14 11:42:33 +0300

    Initial Commit

---
+++ AsyncImage/AsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImageApp.swift
+//  AsyncImage
+//
+//  Created by ramil on 14.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
